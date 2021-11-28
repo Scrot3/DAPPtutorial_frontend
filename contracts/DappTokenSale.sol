f7f7550a3dcf41848c6dea51202c34762da22b41
Albert Chung <jaybhum@gmail.com>
2021-11-28 14:43:58 -0800

    Token Sale

---
+++ contracts/DappTokenSale.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.8.0;
+
+
+/* How it works
+1. Provision tokens to token sale contract
+2. Set a token price in Wei
+3. Assign an admin
+4. Buy tokens
+5. End sale
+*/
+
+import "./DappToken.sol";
+
+contract DappTokenSale {
+	address admin; // admin is a state variable, which belongs to the contract, written to blockchain
+	// DappToken public tokenContract; // tokenContract is of datatype DappToken. This will give us a getter with the same name since it is a public variable.
+	DappToken public tokenContract;
+	uint256 public tokenPrice;
+	uint256 public tokensSold;
+
+	event Sell(address _buyer, uint256 _amount);
+
+	constructor(DappToken _tokenContract, uint256 _tokenPrice) public {
+		// Assign an admin (external account connected to blockchain who has special privileges. e.g. end token sale)
+		admin = msg.sender; // who deploys the contract is the admin.
+
+		// Add reference to Token Contract
+		tokenContract = _tokenContract;
+
+		// Set Token price 
+		tokenPrice = _tokenPrice;
+	}
+
+	// multiply function, sourced from: github.com/dapphub/ds-math/blob/master/src/math.sol
+	function multiply(uint x, uint y) internal pure returns (uint z) { // internal, so it is only visible inside contract. pure, so it is not reading or writing data to blockchain
+		require(y == 0 || (z = x * y) / y == x);
+	}
+
+
+	// Buy Tokens
+	function buyTokens(uint256 _numberOfTokens) public payable { // 'payable' for someone to be able to send ether for the transaction.
+
+		// Require that value is equal to tokens *** hard
+		require(msg.value == multiply(_numberOfTokens, tokenPrice)); // msg.value is the amount in wei that sender is sending.
+
+		// Require that the contract has enough tokens *** hard
+		// first, need to provision enough tokens to token sale contract.
+		require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+		// Require that a transfer is successful *** hard
+		require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+		// Keep track of token sold
+		tokensSold += _numberOfTokens;
+
+		// Emit sell event
+		emit Sell(msg.sender, _numberOfTokens);
+	}
+
+
+}
+
+
+
+
+
---
+++ test/DappTokenSale.js
@@ -0,0 +1,66 @@
+var DappToken = artifacts.require("DappToken.sol");
+var DappTokenSale = artifacts.require("DappTokenSale.sol");
+
+contract('DappTokenSale', function(accounts) {
+	var tokenSale;
+	var tokenSaleInstance;
+	var admin = accounts[0];
+	var buyer = accounts[1];
+	var tokenPrice = 1000000000000000; // in wei
+	var tokensAvaliable = 750000;
+	var numberOfTokens;
+
+	it('initializes the contract with the correct values' ,function(){
+		return DappTokenSale.deployed().then(function(instance){
+			tokenSaleInstance = instance;
+			return tokenSaleInstance.address;
+		}).then(function(address){
+			assert.notEqual(address, 0x0,'has contract address');
+			return tokenSaleInstance.tokenContract();
+		}).then(function(address){
+			assert.notEqual(address, 0x0,'has token contract address');
+			return tokenSaleInstance.tokenPrice();
+		}).then(function(price){
+			assert.equal(price, tokenPrice, 'token price is correct');
+		});
+	});
+
+	it('facilitates token buying', function() {
+		return DappToken.deployed().then(function(instance){
+			// Grab token instance first
+			tokenInstance = instance;
+			return DappTokenSale.deployed();
+		}).then(function(instance){
+			// Then grab token sale instance
+			tokenSaleInstance = instance;
+			// Provision 75% of all tokens to the token sale
+			return tokenInstance.transfer(tokenSaleInstance.address, tokensAvaliable, { from: admin });
+		}).then(function(receipt){
+
+			numberOfTokens = 10;
+			return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: numberOfTokens * tokenPrice}); // Meta data in {}
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+			assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+			assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+			
+			return tokenSaleInstance.tokensSold();
+		}).then(function(amount){
+			assert.equal(amount.toNumber(), numberOfTokens, 'increments the number of tokens sold');
+			return tokenInstance.balanceOf(buyer);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), numberOfTokens);
+			return tokenInstance.balanceOf(tokenSaleInstance.address);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), tokensAvaliable - numberOfTokens);
+			// Try to buy tokens different from ether value
+			return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1  } ); // Try buying token for 1 wei.
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+			return tokenSaleInstance.buyTokens(800000, { from: buyer, value: 800000 * tokenPrice  } ); // Try buying token for 1 wei.
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available'); // Whatever error happens before, it resorts to this error message... hm... not ideal...
+		});
+	});
+})
