b147af4ec0b156187b3e3eae8b645f6d0a528047
Albert Chung <jaybhum@gmail.com>
2021-11-28 11:04:44 -0800

    Token price

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.2 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract DappToken {
 	// Name
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 var DappToken = artifacts.require("DappToken");
+var DappTokenSale = artifacts.require("DappTokenSale");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+
+    var tokenPrice = 1000000000000000; // in wei
+    return deployer.deploy(DappTokenSale,DappToken.address, tokenPrice);
+  })
+  
 };
